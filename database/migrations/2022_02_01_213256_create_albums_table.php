3ac4344f682ce789a300a4aad2481d440560d7e5
eosia <kevin.eosiaweb@gmail.com>
2022-02-20 17:27:10 +0100

    Correction migrations

--- database/migrations/2022_02_01_213256_create_albums_table.php
+++ database/migrations/2022_02_01_213256_create_albums_table.php
@@ -17,7 +17,7 @@ class CreateAlbumsTable extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
-            $table->string('slug')->nullable();
+            $table->string('slug');
             $table->text('description');
             $table->timestamps();
         });
