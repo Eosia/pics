aa9421c7e33e0e49486b04255edabfd4db8b2381
eosia <kevin.eosiaweb@gmail.com>
2022-04-03 14:47:57 +0200

    Intégration vue album

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -2,27 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\{ Album, Category, Tag };
+use App\Models\{
+    Album, Category, Tag, Photo
+};
 use Illuminate\Http\Request;
+
 use App\Http\Requests\AlbumRequest;
+
 use DB, Auth, Storage, Cache;
+
+use App\Services\PhotoService;
 
 class AlbumController extends Controller
 {
+    public function __construct(){
+        $this->middleware(['auth', 'verified'])->except('show');
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function __construct() {
-        $this->middleware(['auth', 'verified'])->except('show');
-    }
-
-    public function index() { // liste des albums de l'user connecté
-        //dd(auth()->user()->name);
-        $albums = auth()->user()->albums()->with('photos', fn($query) => $query->withoutGlobalScope('active')->orderByDesc('created_at'))->orderByDesc('updated_at')->paginate();
-
-        //dd($albums);
+    public function index() //liste des albums de l'utilisateur connecté
+    {
+        $albums = auth()->user()->albums()
+            ->with('photos', fn($query) => $query->withoutGlobalScope('active')->orderByDesc('created_at'))->orderByDesc('updated_at')->paginate();
 
         $data = [
             'title' => $description = 'Mes albums',
@@ -30,7 +34,6 @@ class AlbumController extends Controller
             'albums' => $albums,
             'heading' => $description,
         ];
-
         return view('album.index', $data);
     }
 
@@ -54,43 +57,45 @@ class AlbumController extends Controller
     public function store(AlbumRequest $request)
     {
         DB::beginTransaction();
-        try {
+
+        try{
             $album = Auth::user()->albums()->create($request->validated());
-            //dd($request->categories);
-            $categories = explode(',',$request->categories);
+
+            $categories = explode(',', $request->categories);
+
             $categories = collect($categories)->filter(function($value, $key){
-                return $value != '' && $value != ' ';
+                return $value != ' ';
             })->all();
 
-            //dd($categories);
-            foreach($categories as $cat) {
-                $category = Category::firstOrCreate(['name' => ucfirst(trim($cat))]);
+            foreach($categories as $cat){
+                $category = Category::firstOrCreate(['name' => trim($cat)]);
                 $album->categories()->attach($category->id);
             }
 
-            $tags = explode(',',$request->tags);
+            $tags = explode(',', $request->tags);
+
             $tags = collect($tags)->filter(function($value, $key){
-                return $value != '' && $value != ' ';
+                return $value != ' ';
             })->all();
 
-            //dd($tags);
-            foreach($tags as $t) {
-                $tag = Tag::firstOrCreate(['name' => ucfirst(trim($t))]);
+            foreach($tags as $t){
+                $tag = Tag::firstOrCreate(['name' => trim($t)]);
                 $album->tags()->attach($tag->id);
             }
         }
-        catch(ValidationException $e) {
+        catch(ValidationException $e){
             DB::rollBack();
             dd($e->getErrors());
         }
 
+
         DB::commit();
 
+        $success = 'Album ajouté.';
         $redirect = route('photos.create', [$album->slug]);
-        $success = 'Album ajouté';
-        return $request->ajax() ?
-            response()->json(['success' => $success, 'redirect' => $redirect]) :
-            redirect($redirect)->withSuccess($success);
+        return $request->ajax()
+            ? response()->json(['success' => $success, 'redirect' => $redirect])
+            : redirect($redirect)->withSuccess($success);
     }
 
     /**
@@ -101,7 +106,21 @@ class AlbumController extends Controller
      */
     public function show(Album $album)
     {
-        //
+        $album->load('photos');
+        $sort = request()->query('sort', null);
+        $query = Photo::query()->whereAlbumId($album->id)->with('album.user.photos');
+
+        $currentPage = http_build_query(request()->query());
+
+        $photos = Cache::rememberForever('photos_album_'.$album->id.'_'.$currentPage, fn() => (new PhotoService())->getAll($query, $sort));
+
+        $data = [
+            'title'=>$album->title.' - '.config('app.name'),
+            'description'=>$album->photos->count().' photo(s) dans l\'album '.$album->title,
+            'heading'=>$album->title,
+            'photos'=>$photos,
+        ];
+        return view('album.show', $data);
     }
 
     /**
@@ -112,7 +131,20 @@ class AlbumController extends Controller
      */
     public function edit(Album $album)
     {
-        //
+        abort_if($album->user_id !== auth()->id(), 403);
+        $album->load('categories:name,slug', 'tags:name,slug');
+        $categories = $album->categories->implode('name', ', ');
+        $tags = $album->tags->implode('name', ', ');
+
+        $data = [
+            'title' => $description = 'Editer '.$album->title,
+            'description' => $description,
+            'album' => $album,
+            'heading' => 'Mettre à jour '.$album->title,
+            'categories' => $categories,
+            'tags' => $tags,
+        ];
+        return view('album.edit', $data);
     }
 
     /**
@@ -122,9 +154,56 @@ class AlbumController extends Controller
      * @param  \App\Models\Album  $album
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Album $album)
+    public function update(AlbumRequest $request, Album $album)
     {
-        //
+        abort_if($album->user_id !== auth()->id(), 403);
+
+        DB::beginTransaction();
+        try{
+            $album = Auth::user()->albums()->updateOrCreate(
+                ['id' => $album->id],
+                $request->validated()
+            );
+
+            $categories = explode(',', $request->categories);
+            $categories = collect($categories)->filter(function($value, $key){
+                return $value != ' ';
+            })->all();
+
+            $categoryIds = [];
+            foreach($categories as $cat){
+                $category = Category::firstOrCreate(['name' => trim($cat)]);
+                array_push($categoryIds, $category->id);
+            }
+
+            $album->categories()->sync($categoryIds);
+
+
+            $tags = explode(',', $request->tags);
+            $tags = collect($tags)->filter(function($value, $key){
+                return $value != ' ';
+            })->all();
+
+            $tagIds = [];
+            foreach($tags as $t){
+                $tag = Tag::firstOrCreate(['name' => trim($t)]);
+                array_push($tagIds, $tag->id);
+            }
+
+            $album->tags()->sync($tagIds);
+
+        }
+        catch(ValidationException $e){
+            DB::rollBack();
+            dd($e->getErrors());
+        }
+        DB::commit();
+
+        $success = 'Album mis à jour.';
+        $redirect = route('albums.edit', [$album->slug]);
+        return $request->ajax() ?
+            response()->json(['success' => $success, 'redirect' => $redirect])
+            : redirect($redirect)->withSuccess($success);
     }
 
     /**
